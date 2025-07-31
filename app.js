22ef74d394db7b24eb11930b130b35c195c4e0ae
Elmurod17 <turduboyevelmurod7@gmail.com>
2025-07-31 10:20:20 +0500

    3oy9vazifa

---
+++ app.js
@@ -0,0 +1,204 @@
+// 1-masala
+
+// function countdown(n) {
+//   while (n >= 0) {
+//     console.log(n);
+//     n--;
+//   }
+// }
+// countdown(5);
+
+// 2-masala
+
+// function printEven(arr) {
+//   let i = 0;
+//   while (i < arr.length) {
+//     if (arr[i] % 2 == 0) {
+//       console.log(arr[i]);
+//     }
+//     i++;
+//   }
+// }
+// printEven([1, 2, 3, 4, 5, 6]);
+
+// 3-masala
+
+// function countA(str) {
+//   let i = 0;
+//   let count = 0;
+//   while (i < str.length) {
+//     if (str[i] === "a") {
+//       count++;
+//     }
+//     i++;
+//   }
+//   return count;
+// }
+// console.log(countA("salom"));
+
+// 4-masala
+
+// function sumArray(arr) {
+//   let i = 0;
+//   let sum = 0;
+//   while (i < arr.length) {
+//     sum += arr[i];
+//     i++;
+//   }
+//   return sum;
+// }
+// console.log(sumArray([3, 2, 1]));
+
+// 5-masala
+
+// function reverseText(str) {
+//   let i = str.length - 1;
+//   let natija = "";
+//   while (i >= 0) {
+//     natija += str[i];
+//     i--;
+//   }
+//   return natija;
+// }
+// console.log(reverseText("hi"));
+
+// 6-masala
+
+// function printNegative(arr) {
+//   let i = 0;
+//   while (i < arr.length) {
+//     if (arr[i] < 0) {
+//       console.log(arr[i]);
+//     }
+//     i++;
+//   }
+// }
+// printNegative([-1, 2, -3]);
+
+// 7-masala
+
+// function countChars(str) {
+//   let i = 0;
+//   let count = 0;
+//   while (i < str.length) {
+//     count++;
+//     i++;
+//   }
+//   return count;
+// }
+// console.log(countChars("hello"));
+
+// 8-masala
+
+// function printOdds(n) {
+//   let i = 0;
+//   while (i <= n) {
+//     if (i % 2 === 1) {
+//       console.log(i);
+//     }
+//     i++;
+//   }
+// }
+// printOdds(5);
+
+// 9-masala
+
+// function findMax(arr) {
+//   let i = 0;
+//   let max = arr[0];
+//   while (i < arr.length) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//     i++;
+//   }
+//   return max;
+// }
+// console.log(findMax([1, 9, 3]));
+
+// 10-masaal
+
+// function countO(str) {
+//   let i = 0;
+//   let count = 0;
+//   while (i < str.length) {
+//     if (str[i] === "o") {
+//       count++;
+//     }
+//     i++;
+//   }
+//   return count;
+// }
+// console.log(countO("hello world"));
+
+// 11-masala
+
+// function countZeros(arr) {
+//   let i = 0;
+//   let count = 0;
+//   while (i < arr.length) {
+//     if (arr[i] === 0) {
+//       count++;
+//     }
+//     i++;
+//   }
+//   return count;
+// }
+// console.log(countZeros([0, 1, 0]));
+
+// 12-masala
+
+// function sumPositive(arr) {
+//   let i = 0;
+//   let sum = 0;
+//   while (i < arr.length) {
+//     if (arr[i] > 0) {
+//       sum += arr[i];
+//     }
+//     i++;
+//   }
+//   return sum;
+// }
+// console.log(sumPositive([-1, 2, 3]));
+
+// 13-masala
+
+// function printLowercase(str) {
+//   let i = 0;
+//   while (i < str.length) {
+//     let harf = str[i];
+//     if (harf === harf.toLowerCase() && harf !== harf.toUpperCase()) {
+//       console.log(harf);
+//     }
+//     i++;
+//   }
+// }
+
+// 14-masala
+
+// function printOddIndexes(arr) {
+//   let i = 0;
+//   while (i < arr.length) {
+//     if (i % 2 === 1) {
+//       console.log(arr[i]);
+//     }
+//     i++;
+//   }
+// }
+// printOddIndexes(["a", "b", "c", "d"]);
+
+// 15-masala
+
+// function replaceE(str) {
+//   let i = 0;
+//   let result = "";
+//   while (i < str.length) {
+//     if (str[i] === "e") {
+//       result += "3";
+//     } else {
+//       result += str[i];
+//     }
+//     i++;
+//   }
+//   return result;
+// }
